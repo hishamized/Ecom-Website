d7f3507246917c02ae9b328538d6752f85f0c454
hishamized <mirhashim2001@gmail.com>
2022-03-29 00:55:04 +0530

    Products fetched from Database directly instead of static Html

--- Mobile Shopee/functions.php
+++ Mobile Shopee/functions.php
@@ -1,7 +1,15 @@
 <?php
-//require mysql connection
-require('database/DBController.php');
 
-//DB Controller Object
+// require MySQL Connection
+require ('database/DBController.php');
+
+// require Product Class
+require ('database/Product.php');
+
+
+// DBController object
 $db = new DBController();
-?>
+
+// Product object
+$product = new Product($db);
+
