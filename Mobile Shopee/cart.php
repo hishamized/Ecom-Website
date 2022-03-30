3ac2964f857a8de982556a061b204707bfae878f
hishamized <mirhashim2001@gmail.com>
2022-03-30 18:55:06 +0530

    Cart added and Products fetched with Database

--- Mobile Shopee/cart.php
+++ Mobile Shopee/cart.php
@@ -1,22 +1,22 @@
 <?php
-//include header.php file
+// include header.php file
 include ('header.php');
-//include header.php file
 ?>
 
 <?php
-//include cart template
-include('Template/_cart-template.php');
-//include cart template
 
-//include new phones
+/*  include top sale section */
+include ('Template/_cart-template.php');
+/*  include top sale section */
+
+/*  include top sale section */
 include ('Template/_new-phones.php');
-//include new phones
-?>
+/*  include top sale section */
 
+?>
 
 <?php
-//include footer.php file
+// include footer.php file
 include ('footer.php');
-//include footer.php file
 ?>
+
