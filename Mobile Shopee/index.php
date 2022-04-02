a1837d45dbbe6537569385fedc2331ca5a497c5d
hishamized <mirhashim2001@gmail.com>
2022-04-02 15:21:52 +0530

    Backend Completed

--- Mobile Shopee/index.php
+++ Mobile Shopee/index.php
@@ -1,39 +1,39 @@
 <?php
-   ob_start();
-   //include header.php file
-   include ('header.php');
-//include header.php file
+ob_start();
+// include header.php file
+include ('header.php');
 ?>
 
 <?php
-   //include banner area
-   include ('Template/_banner-area.php');
-//include banner area
 
-//include top sale
+/*  include banner area  */
+include ('Template/_banner-area.php');
+/*  include banner area  */
+
+/*  include top sale section */
 include ('Template/_top-sale.php');
-//include top sale
+/*  include top sale section */
 
-//include special price
+/*  include special price section  */
 include ('Template/_special-price.php');
-//include special price
+/*  include special price section  */
 
-//include banner ads
+/*  include banner ads  */
 include ('Template/_banner-ads.php');
-//include banner ads
+/*  include banner ads  */
 
-//include new phones
+/*  include new phones section  */
 include ('Template/_new-phones.php');
-//include new phones
+/*  include new phones section  */
 
-//include new phones
+/*  include blog area  */
 include ('Template/_blogs.php');
-//include new phones
+/*  include blog area  */
+
 ?>
 
 
 <?php
-//include footer.php file
+// include footer.php file
 include ('footer.php');
-//include footer.php file
 ?>
